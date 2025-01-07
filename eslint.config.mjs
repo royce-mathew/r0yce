502fb6e67178bffcd4cb0175157251279c4def21
royce-mathew <royce1mathew@gmail.com>
2025-01-06 20:33:54 -0500

    chore: upgrade to eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import tailwindcss from "eslint-plugin-tailwindcss";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends(
+    "next/core-web-vitals",
+    "prettier",
+    "next",
+    "plugin:tailwindcss/recommended",
+), {
+    plugins: {
+        tailwindcss,
+    },
+
+    settings: {
+        tailwindcss: {
+            callees: ["cn", "cva"],
+            config: "tailwind.config.cjs",
+        },
+    },
+
+    rules: {
+        "@next/next/no-html-link-for-pages": "off",
+        "tailwindcss/no-custom-classname": "off",
+        "tailwindcss/classnames-order": "error",
+    },
+}, {
+    files: ["**/*.ts", "**/*.tsx"],
+
+    languageOptions: {
+        parser: tsParser,
+    },
+}];
