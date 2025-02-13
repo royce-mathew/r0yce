3a3b81baaae2b38f7e87854b0b66a1f8ce748ff1
royce-mathew <royce1mathew@gmail.com>
2025-02-13 16:08:34 -0500

    fix: wordle, pull from gist instead

--- src/app/(app)/apps/wordle/get-word.ts
+++ src/app/(app)/apps/wordle/get-word.ts
@@ -1,28 +1,26 @@
 "use server"
 
-import fs from "fs/promises"
-import path from "path"
-
 let validWords: string[] | null = null
 let allWords: string[] | null = null
 
+const allWordsGistUrl =
+  "https://gist.githubusercontent.com/royce-mathew/ff9bb6455fbd4f7cff532b2a5b4900c4/raw/possible-words.txt"
+const validWordsGistUrl =
+  "https://gist.githubusercontent.com/royce-mathew/50f4a64ee7ea9793fa00c903ea4e7dcf/raw/words.txt"
+
 async function loadWords() {
   if (!validWords) {
-    const wordsFile = await fs.readFile(
-      path.join(process.cwd(), "public/files/words.txt"),
-      "utf8"
-    )
+    const response = await fetch(validWordsGistUrl)
+    const wordsFile = await response.text()
     validWords = wordsFile.split("\n").map((word) => word.trim())
   }
 }
 
 async function loadAllWords() {
   if (!allWords) {
-    const allWordsFile = await fs.readFile(
-      path.join(process.cwd(), "public/files/possible-words.txt"),
-      "utf8"
-    )
-    allWords = allWordsFile.split("\n").map((word) => word.trim())
+    const response = await fetch(allWordsGistUrl)
+    const wordsFile = await response.text()
+    allWords = wordsFile.split("\n").map((word) => word.trim())
   }
 }
 
