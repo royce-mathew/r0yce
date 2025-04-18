aa4272939ff9570c7b47b551bd1ee0474403e3d7
Royce Mathew <114615781+royce-mathew@users.noreply.github.com>
2025-04-18 08:40:12 -0400

    fix!: rehype upgrade (#40)
    * chore: remove unused dependency
    
    * chore: reverse built dependencies
    
    * chore: clean install lockfile
    
    * fix: specific version patch for rehype-pretty-code
    
    * fix!: rehype upgrade
    The rehype patch for upgrading to shikiv3 is not needed anymore
    
    * chore: reinitialize pnpm-lock

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import path from "node:path"
 import { fileURLToPath } from "node:url"
 import { FlatCompat } from "@eslint/eslintrc"
 import js from "@eslint/js"
-import tsParser from "@typescript-eslint/parser"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
